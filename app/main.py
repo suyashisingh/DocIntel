db7da06ce532aa206f10022698b69141ff56d483
suyashisingh <suyashisinghh@email.com>
2026-07-06 16:38:58 +0530

    Fix CORS to use configured FRONTEND_URL instead of hardcoded localhost

--- app/main.py
+++ app/main.py
@@ -304,7 +304,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174"],
+    allow_origins=[settings.FRONTEND_URL, "http://localhost:5173", "http://localhost:5174"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
