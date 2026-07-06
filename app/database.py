1f8d4c544b1accd8c0ce96876adf9b272b6c7387
suyashisingh <suyashisinghh@email.com>
2026-07-06 17:50:43 +0530

    Add pool_pre_ping and pool_recycle to fix stale Neon connections after suspend

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True, pool_recycle=300)
 
 SessionLocal = sessionmaker(
     autocommit=False,
